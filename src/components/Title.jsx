f0b07cfe0cd62eb6948668aa6e8fb1585e7dd386
Jiyad <mdjiyade@gmail.com>
2025-09-09 12:36:57 +0530

    Added text-center

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Title = ({ title, className }) => {
-    return <h2 className={`text-primary font-bold ${className}`}>{title}</h2>
+    return <h2 className={`text-primary text-center font-bold ${className}`}>{title}</h2>
 }
 
 export default Title
