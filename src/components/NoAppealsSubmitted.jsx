76cb85fba221acdd89dd52035f17649a1c2ed268
Jiyad <mdjiyade@gmail.com>
2025-12-28 12:29:02 +0530

    Add max-width to NoAppealsSubmitted component

--- src/components/NoAppealsSubmitted.jsx
+++ src/components/NoAppealsSubmitted.jsx
@@ -3,20 +3,22 @@ import Button from "./Button"
 
 const NoAppealsSubmitted = () => {
     return (
-        <div className='flex flex-col px-3 py-4 gap-4 flex-1 items-center justify-center'>
-            <LuScroll className='text-8xl' />
-            <h2 className='text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black w-[20ch]'>
-                No Appeals Submitted
-            </h2>
-            <p className='text-center text-secondary-light dark:text-secondary-dark w-[50ch]'>
-                You haven't submitted any appeals yet. If you believe an issue
-                requires review, you can submit an appeal
-            </p>
-            <Button
-                text='Submit Appeal'
-                className='px-6 py-3 text-sm font-medium bg-accent hover:bg-button-hover'
-                onClick={() => {}}
-            />
+        <div className='flex flex-col px-3 py-4 flex-1 items-center justify-center'>
+            <div className='flex flex-col px-3 py-4 gap-4 flex-1 items-center justify-center max-w-xl'>
+                <LuScroll className='text-8xl' />
+                <h2 className='text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black w-[20ch]'>
+                    No Appeals Submitted
+                </h2>
+                <p className='text-sm md:text-base text-center text-secondary-light dark:text-secondary-dark'>
+                    You haven't submitted any appeals yet. If you believe an
+                    issue requires review, you can submit an appeal
+                </p>
+                <Button
+                    text='Submit Appeal'
+                    className='px-6 py-3 text-sm font-medium bg-accent hover:bg-button-hover'
+                    onClick={() => {}}
+                />
+            </div>
         </div>
     )
 }
