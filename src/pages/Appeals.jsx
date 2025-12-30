0200c306c3c6c33222ce942dfdd7165641b155c7
Jiyad <mdjiyade@gmail.com>
2025-12-30 13:56:51 +0530

    Added sort and filter for admin

--- src/pages/Appeals.jsx
+++ src/pages/Appeals.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { useBlocker, useLocation, useNavigate } from "react-router-dom"
 import NoAppealsSubmitted from "../components/NoAppealsSubmitted"
 import SubmitAppealFormModal from "../components/SubmitAppealFormModal"
@@ -6,13 +6,23 @@ import CancelConfirm from "../components/CancelConfirm"
 import api from "../api/api"
 import { useOutletContext } from "react-router-dom"
 import toast from "react-hot-toast"
+import AppealsList from "../components/AppealsList"
+import AppealsHeader from "../components/AppealsHeader"
+import { useAuthStore, useElectionStore } from "../stores"
 
 const Appeals = () => {
     const [appeals, setAppeals] = useState([])
     const [showAppealForm, setShowAppealForm] = useState(false)
     const [isCancelConfirmOpen, setIsCancelConfirmOpen] = useState(false)
+    const [sort, setSort] = useState("latest")
+    const [filter, setFilter] = useState("all")
 
-    const { setIsLoading } = useOutletContext()
+    const { isLoading, setIsLoading } = useOutletContext()
+
+    const { election } = useElectionStore()
+    const {
+        user: { role }
+    } = useAuthStore()
 
     const navigate = useNavigate()
     const location = useLocation()
@@ -42,8 +52,12 @@ const Appeals = () => {
         const fetchAppeals = async () => {
             try {
                 setIsLoading(true)
-                const res = await api.get("/appeals")
-                setAppeals(res.data)
+                if (election.id) {
+                    const res = await api.get(
+                        `/appeals?election=${election.id}`
+                    )
+                    setAppeals(res.data)
+                }
             } catch (err) {
                 toast.error(
                     err.response?.data?.error ||
@@ -56,13 +70,58 @@ const Appeals = () => {
         }
 
         fetchAppeals()
-    }, [setIsLoading])
+    }, [setIsLoading, election.id])
+
+    const visibleAppeals = useMemo(() => {
+        let list = [...appeals]
+
+        // sort
+        if (sort === "oldest") {
+            list.sort((a, b) => new Date(a.created_at) - new Date(b.created_at))
+        } else if (sort === "latest") {
+            list.sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
+        }
+
+        // filter by category
+        if (filter !== "all") {
+            list = list.filter((l) => l.category.toLowerCase() === filter)
+        }
+
+        return list
+    }, [appeals, filter, sort])
+
+    if (isLoading) return null
 
     return (
-        <div className='flex flex-col px-2 md:px-5 lg:px-9 py-5 flex-1'>
+        <div className='flex flex-col items-center px-2 md:px-5 lg:px-9 py-5 flex-1'>
             <title>Appeals</title>
-            {appeals.length === 0 && (
-                <NoAppealsSubmitted setShowAppealForm={setShowAppealForm} />
+            {visibleAppeals.length === 0 && !isLoading && (
+                <>
+                    {role === "admin" ? (
+                        <div className='flex px-3 py-4 flex-1 items-center justify-center'>
+                            <h2 className='text-center text-primary-light dark:text-primary-dark text-2xl md:text-3xl lg:text-4xl font-black w-[20ch]'>
+                                No Appeals To Show
+                            </h2>
+                        </div>
+                    ) : (
+                        <NoAppealsSubmitted
+                            setShowAppealForm={setShowAppealForm}
+                        />
+                    )}
+                </>
+            )}
+
+            {visibleAppeals.length > 0 && (
+                <div className='flex flex-col w-full gap-5 max-w-5xl py-2'>
+                    <AppealsHeader
+                        setShowAppealForm={setShowAppealForm}
+                        sort={sort}
+                        setSort={setSort}
+                        setFilter={setFilter}
+                        filter={filter}
+                    />
+                    <AppealsList appeals={visibleAppeals} />
+                </div>
             )}
 
             {showAppealForm && (
@@ -70,6 +129,7 @@ const Appeals = () => {
                     isOpen={showAppealForm}
                     setIsCancelConfirmOpen={setIsCancelConfirmOpen}
                     setShowAppealForm={setShowAppealForm}
+                    setAppeals={setAppeals}
                 />
             )}
 
