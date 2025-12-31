cce536783dde92ad60f3a40b25915be1b6c94d6d
Jiyad <mdjiyade@gmail.com>
2025-12-31 11:12:31 +0530

    Blocked scroll when FullScreenLoader is displayed

--- src/components/FullScreenLoader.jsx
+++ src/components/FullScreenLoader.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react"
 import Loader from "./Loader"
 
 const FullScreenLoader = ({ suspense }) => {
+    useEffect(() => {
+        document.body.classList.add("overflow-hidden")
+
+        return () => {
+            document.body.classList.remove("overflow-hidden")
+        }
+    }, [])
+
     return (
         <div
-            className={`flex justify-center items-center z-50 inset-0 h-screen ${
+            className={`flex justify-center items-center z-50 inset-0 h-screen scroll- ${
                 suspense
                     ? "bg-bg-light dark:bg-bg-dark"
                     : "bg-bg-light/70 dark:bg-bg-dark/70"
