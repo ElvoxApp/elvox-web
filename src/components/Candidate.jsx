834cec83f8cd2696ebd19e320346b376ef4dcffb
Jiyad <mdjiyade@gmail.com>
2026-01-02 08:20:26 +0530

    Redesigned ViewCandidates page

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -13,32 +13,29 @@ const readableDate = (date) => {
 
 const Candidate = ({ candidate }) => {
     return (
-        <div className='flex flex-col items-center justify-center bg-card-light dark:bg-card-dark rounded-lg px-3 py-4 gap-4'>
-            <div className='flex flex-col gap-2 items-center'>
+        <div className='flex items-cente gap-3 dark:bg-[#16171d] bg-bg-light text-primary-light dark:text-primary-dark rounded-md px-3 py-2'>
+            <div className='flex items-center'>
                 <img
                     src={candidate?.profile_pic}
                     alt={candidate?.name}
-                    className='w-18 md:w-24 lg:w-24 rounded-full border border-accent'
+                    className='w-14 md:w-18 rounded-full border border-accent'
                 />
-                <p className='text-sm md:text-base text-primary-light dark:text-primary-dark text-center'>
-                    {candidate?.name}
-                </p>
             </div>
-            <div className='flex gap-4 sm:gap-2 md:gap-3 lg:gap-4 justify-center text-xs text-primary-light dark:text-primary-dark'>
-                <p className='border border-accent rounded-lg py-2 px-3 text-center break-words'>
-                    {getYear(candidate?.semester)}
-                </p>
-                <p className='border border-accent rounded-lg py-2 px-3 text-center break-words'>
-                    {candidate?.class}
-                </p>
-                <p className='border border-accent rounded-lg py-2 px-3 text-center break-words'>
-                    {candidate?.category}
-                </p>
-            </div>
-            <div className='flex justify-center items-center'>
-                <p className='text-secondary-light dark:text-secondary-dark text-xs text-center'>
-                    Approved on: {readableDate(candidate.updated_at)}
-                </p>
+            <div className='flex flex-col justify-center gap-1'>
+                <div className='flex items-center gap-2'>
+                    <p className='text-sm md:text-base text-primary-light dark:text-primary-dark text-center'>
+                        {candidate?.name}
+                    </p>
+                </div>
+                <div className='flex items-center gap-2'>
+                    <p className='text-xs'>{candidate?.class},</p>
+                    <p className='text-xs'>{getYear(candidate?.semester)}</p>
+                </div>
+                <div className='flex'>
+                    <p className='text-secondary-light dark:text-secondary-dark text-xs text-center'>
+                        Approved on: {readableDate(candidate.updated_at)}
+                    </p>
+                </div>
             </div>
         </div>
     )
