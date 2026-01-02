c80a2a35f9f8582edc4ccde5746c7a62e833a81e
Jiyad <mdjiyade@gmail.com>
2026-01-02 16:35:08 +0530

    Increased success toast duration

--- src/App.jsx
+++ src/App.jsx
@@ -114,7 +114,7 @@ const App = () => {
                         duration: 3000,
                         removeDelay: 1000,
                         success: {
-                            duration: 2000
+                            duration: 2500
                         }
                     }}
                 />
