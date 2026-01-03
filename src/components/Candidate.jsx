56fb4113c188d36accd4574505e0c53025adcdc7
Jiyad <mdjiyade@gmail.com>
2026-01-03 10:51:30 +0530

    Removed absolute and added grid for mobile responsiveness

--- src/components/Candidate.jsx
+++ src/components/Candidate.jsx
@@ -15,8 +15,8 @@ const readableDate = (date) => {
 
 const Candidate = ({ candidate }) => {
     return (
-        <div className='flex items-center justify-between gap-3 dark:bg-[#16171d] bg-bg-light text-primary-light dark:text-primary-dark rounded-md px-3 py-2 relative'>
-            <div className='flex gap-3'>
+        <div className='flex items-center justify-between gap-3 dark:bg-[#16171d] bg-bg-light text-primary-light dark:text-primary-dark rounded-md px-3 py-2'>
+            <div className='flex gap-3 max-sm:flex-1'>
                 <div className='flex items-center'>
                     <img
                         src={candidate?.profile_pic}
@@ -24,13 +24,16 @@ const Candidate = ({ candidate }) => {
                         className='w-12 md:w-16 rounded-full p-1'
                     />
                 </div>
-                <div className='flex flex-col justify-center gap-1'>
-                    <div className='flex items-center gap-2'>
+                <div className='flex flex-col justify-center gap-1 max-sm:flex-1'>
+                    <div className='flex items-center max-sm:justify-between gap-2'>
                         <p className='text-sm md:text-base text-primary-light dark:text-primary-dark text-center'>
                             {candidate?.name}
                         </p>
+                        <span className='bg-green-400/30 dark:bg-green-400/20 text-green-500 dark:text-green-400 ring-1 ring-green-400/30 inline-block px-3 py-1 rounded-xl text-xs font-medium sm:hidden'>
+                            {capitalize(candidate?.status)}
+                        </span>
                     </div>
-                    <div className='flex max-sm:flex-col gap-0.5 sm:items-center sm:gap-2 max-sm:pb-3'>
+                    <div className='flex max-sm:flex-col gap-0.5 sm:items-center sm:gap-2'>
                         <p className='text-xs'>
                             {candidate?.class}
                             <span className='max-sm:hidden'>,</span>
@@ -39,14 +42,15 @@ const Candidate = ({ candidate }) => {
                             {getYear(candidate?.semester)}
                         </p>
                     </div>
-                    <div className='flex'>
-                        <p className='text-secondary-light dark:text-secondary-dark text-xs text-center max-sm:absolute max-sm:bottom-1'>
-                            Approved on: {readableDate(candidate.updated_at)}
+                    <div className='grid grid-cols-2 sm:flex'>
+                        <p className='text-secondary-light dark:text-secondary-dark text-xs sm:text-center flex max-sm:flex-col'>
+                            <span>Approved on: </span>
+                            <span>{readableDate(candidate.updated_at)}</span>
                         </p>
                     </div>
                 </div>
             </div>
-            <div className='flex items-center gap-2'>
+            <div className='flex items-center gap-2 max-sm:hidden'>
                 <span className='bg-green-400/30 dark:bg-green-400/20 text-green-500 dark:text-green-400 ring-1 ring-green-400/30 inline-block px-3 py-1 rounded-xl text-xs font-medium'>
                     {capitalize(candidate?.status)}
                 </span>
