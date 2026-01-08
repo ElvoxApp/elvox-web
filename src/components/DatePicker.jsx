2cec96cb4657179978f7495ca250235d1a9b1607
Jiyad <mdjiyade@gmail.com>
2026-01-08 22:09:03 +0530

    Removed commit function and removed date state and replaced with value from rhf

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -11,20 +11,6 @@ export default function DateTimePicker({
     placeholder = "Select date"
 }) {
     const [open, setOpen] = useState(false)
-    const [date, setDate] = useState(value ?? null)
-
-    const commit = (nextDate, nextTime) => {
-        if (!nextDate || !nextTime) return
-
-        const [h, m] = nextTime.split(":").map(Number)
-        const merged = new Date(nextDate)
-        merged.setHours(h)
-        merged.setMinutes(m)
-        merged.setSeconds(0)
-
-        setDate(merged)
-        onChange?.(merged)
-    }
 
     return (
         <div className='flex-1 sm:flex-2 md:flex-1'>
@@ -37,8 +23,8 @@ export default function DateTimePicker({
                         type='button'
                         className='w-full rounded-md px-3 py-2 text-left text-sm shadow-sm hover:bg-[#c4c4c9] dark:hover:bg-[#2b303b] focus:outline-none cursor-pointer bg-field-light dark:bg-field-dark'
                     >
-                        {date ? (
-                            format(date, "dd MMM yyyy")
+                        {value ? (
+                            format(value, "dd MMM yyyy")
                         ) : (
                             <p className='flex items-center gap-2'>
                                 <MdOutlineCalendarToday className='text-secondary-light dark:text-secondary-dark' />
@@ -61,10 +47,9 @@ export default function DateTimePicker({
                         mode='single'
                         animate
                         navLayout='around'
-                        selected={date}
+                        selected={value}
                         onSelect={(d) => {
-                            setDate(d)
-                            commit(d)
+                            onChange?.(d)
                         }}
                         style={{
                             "--rdp-accent-color": "var(--color-accent)",
