71146ea3e10aee724a10d266464f5c46aca884f1
Jiyad <mdjiyade@gmail.com>
2026-01-10 17:39:24 +0530

    Updated election store set fucntion

--- src/stores/electionStore.js
+++ src/stores/electionStore.js
@@ -2,7 +2,13 @@ import { create } from "zustand"
 
 const useElectionStore = create((set) => ({
     election: {},
-    setElection: (data) => set({ election: data })
+    setElection: (data) =>
+        set((state) => ({
+            election: {
+                ...state.election,
+                ...data
+            }
+        }))
 }))
 
 export default useElectionStore
